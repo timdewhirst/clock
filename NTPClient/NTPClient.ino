987dfaaf376e58532535fab71a73734e7355fdec
Tim Dewhirst <tim@bugless.co.uk>
2023-12-28 15:11:43 +0000

    tidy up, add brightness control

--- NTPClient/NTPClient.ino
+++ NTPClient/NTPClient.ino
@@ -47,16 +47,42 @@ byte packetBuffer[NTP_PACKET_SIZE];  // buffer to hold incoming and outgoing pac
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP udp;
 
-static MATRIX7219 matrix(
-  13, // data pin
-  15, // select pin
-  14, // clock pin
-  4   // matrices
+static const int analogue_pin = A0;
+static const int matrix_data_pin = D7;
+static const int matrix_select_pin = D8;
+static const int matrix_clock_pin = D5;
+static const int matrix_count = 4;
+
+MATRIX7219 matrix(
+  matrix_data_pin,
+  matrix_select_pin,
+  matrix_clock_pin,
+  matrix_count
   );
+
+// keep track of how long since last NTP query
+int ntp_query_time = -1;
+
+// default brightness: 0..15
+int brightness = 3;
+
+bool should_poll_ntp(int now)
+{
+  if ( ntp_query_time == -1 )
+    return true;
+
+  // 10s polling time
+  if ( (now - ntp_query_time) > 10'000 )
+    return true; 
+
+  return false;
+}
 
 void setup() 
 {
+  // setup matrix
   matrix.begin();
+  matrix.clear();
   
   Serial.begin(115200);
   Serial.println();
@@ -86,81 +112,97 @@ void setup()
 
 void loop() 
 {  
-  matrix.clear();
+  int a = analogRead(analogue_pin) / 64;
+  Serial.print("brightness: ");
+  Serial.println(a);
+
+  if ( a != brightness )
+  {
+    brightness = a;
+    matrix.setBrightness(a);
+  }
   matrix.setRow(1, B11111111, 1);
   matrix.setRow(2, B10111101, 2);
   matrix.setRow(3, B11011011, 3);
   matrix.setRow(4, B11100111, 4);
-  
-  // get a random server from the pool
-  WiFi.hostByName(ntpServerName, timeServerIP);
 
-  sendNTPpacket(timeServerIP);  // send an NTP packet to a time server
-
-  int cb = 0;
-  int wait_count = 0;
-  while (!cb && wait_count < 20)
+  // check if we need to update NTP
+  int now = millis();
+  if ( should_poll_ntp(now) )
   {
-    ++wait_count;
-    delay(100); 
-    cb = udp.parsePacket();
+    ntp_query_time = now;
+    
+    // get a random server from the pool
+    WiFi.hostByName(ntpServerName, timeServerIP);
+  
+    sendNTPpacket(timeServerIP);  // send an NTP packet to a time server
+  
+    int cb = 0;
+    int wait_count = 0;
+    while (!cb && wait_count < 20)
+    {
+      ++wait_count;
+      delay(100); 
+      cb = udp.parsePacket();
+    }
+    
+    if (!cb) 
+    {
+      Serial.println("no packet yet");
+      delay(2000);
+    } 
+    else 
+    {
+      Serial.print("packet received, length=");
+      Serial.println(cb);
+      // We've received a packet, read the data from it
+      udp.read(packetBuffer, NTP_PACKET_SIZE);  // read the packet into the buffer
+  
+      // the timestamp starts at byte 40 of the received packet and is four bytes,
+      //  or two words, long. First, extract the two words:
+  
+      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+      // combine the four bytes (two words) into a long integer
+      // this is NTP time (seconds since Jan 1 1900):
+      unsigned long secsSince1900 = highWord << 16 | lowWord;
+      Serial.print("Seconds since Jan 1 1900 = ");
+      Serial.println(secsSince1900);
+  
+      // now convert NTP time into everyday time:
+      Serial.print("Unix time = ");
+      // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+      const unsigned long seventyYears = 2208988800UL;
+      // subtract seventy years:
+      unsigned long epoch = secsSince1900 - seventyYears;
+      // print Unix time:
+      Serial.println(epoch);
+  
+  
+      // print the hour, minute and second:
+      Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+      Serial.print((epoch % 86400L) / 3600);  // print the hour (86400 equals secs per day)
+      Serial.print(':');
+      if (((epoch % 3600) / 60) < 10) {
+        // In the first 10 minutes of each hour, we'll want a leading '0'
+        Serial.print('0');
+      }
+      Serial.print((epoch % 3600) / 60);  // print the minute (3600 equals secs per minute)
+      Serial.print(':');
+      if ((epoch % 60) < 10) {
+        // In the first 10 seconds of each minute, we'll want a leading '0'
+        Serial.print('0');
+      }
+      Serial.println(epoch % 60);  // print the second
+    }
   }
   
-  if (!cb) 
-  {
-    Serial.println("no packet yet");
-    delay(2000);
-  } 
-  else 
-  {
-    Serial.print("packet received, length=");
-    Serial.println(cb);
-    // We've received a packet, read the data from it
-    udp.read(packetBuffer, NTP_PACKET_SIZE);  // read the packet into the buffer
-
-    // the timestamp starts at byte 40 of the received packet and is four bytes,
-    //  or two words, long. First, extract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = ");
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-
-
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    Serial.print((epoch % 86400L) / 3600);  // print the hour (86400 equals secs per day)
-    Serial.print(':');
-    if (((epoch % 3600) / 60) < 10) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch % 3600) / 60);  // print the minute (3600 equals secs per minute)
-    Serial.print(':');
-    if ((epoch % 60) < 10) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60);  // print the second
-  }
-  // wait ten seconds before asking for the time again
-  delay(10000);
+  delay(100);
 }
 
 // send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress& address) {
+void sendNTPpacket(IPAddress& address) 
+{
   Serial.println("sending NTP packet...");
   // set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
