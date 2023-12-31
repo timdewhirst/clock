64848ced5abfb704930dc8e366e95ac939dffdc6
Tim Dewhirst <tim@bugless.co.uk>
2023-12-31 16:28:48 +0000

    refactoring

--- NTPClient/NTPClient.ino
+++ NTPClient/NTPClient.ino
@@ -17,6 +17,97 @@
 const char* ssid = STASSID;  // your network SSID (name)
 const char* pass = STAPSK;   // your network password
 
+// keep track of time
+struct Time
+{
+  uint8_t h {};
+  uint8_t m {};
+  uint8_t s {};
+} 
+current_time;
+int last_update_ms = -1;
+
+unsigned int localPort = 2390;  // local port to listen for UDP packets
+const char* ntpServerName = "time.nist.gov";
+
+const int NTP_PACKET_SIZE = 48;  // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[NTP_PACKET_SIZE];  // buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+bool get_ntp_time()
+{
+  IPAddress timeServerIP;
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP);
+  
+  sendNTPpacket(timeServerIP);  // send an NTP packet to a time server
+  
+  int cb = 0;
+  int wait_count = 0;
+  while (!cb && wait_count < 10)
+  {
+    ++wait_count;
+    delay(100); 
+    cb = udp.parsePacket();
+  }
+  
+  if (!cb) 
+    return false;
+  
+  Serial.print("packet received, length=");
+  Serial.println(cb);
+
+  // We've received a packet, read the data from it
+  udp.read(packetBuffer, NTP_PACKET_SIZE);  // read the packet into the buffer
+
+  // the timestamp starts at byte 40 of the received packet and is four bytes,
+  //  or two words, long. First, extract the two words:
+
+  unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+  unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+  // combine the four bytes (two words) into a long integer
+  // this is NTP time (seconds since Jan 1 1900):
+  unsigned long secsSince1900 = highWord << 16 | lowWord;
+  Serial.print("Seconds since Jan 1 1900 = ");
+  Serial.println(secsSince1900);
+
+  // now convert NTP time into everyday time:
+  Serial.print("Unix time = ");
+  // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+  const unsigned long seventyYears = 2208988800UL;
+  // subtract seventy years:
+  unsigned long epoch = secsSince1900 - seventyYears;
+  // print Unix time:
+  Serial.println(epoch);
+
+  
+
+  // print the hour, minute and second:
+  Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+  Serial.print((epoch % 86400L) / 3600);  // print the hour (86400 equals secs per day)
+  Serial.print(':');
+  if (((epoch % 3600) / 60) < 10) {
+    // In the first 10 minutes of each hour, we'll want a leading '0'
+    Serial.print('0');
+  }
+  Serial.print((epoch % 3600) / 60);  // print the minute (3600 equals secs per minute)
+  Serial.print(':');
+  if ((epoch % 60) < 10) {
+    // In the first 10 seconds of each minute, we'll want a leading '0'
+    Serial.print('0');
+  }
+  Serial.println(epoch % 60);  // print the second
+  return true;
+}
+
+static const int analogue_pin = A0;
+static const int matrix_data_pin = D7;
+static const int matrix_select_pin = D8;
+static const int matrix_clock_pin = D5;
+static const int matrix_count = 5;
+
 /// masks: index is the mask width i.e.
 /// - index 0 -> 0b00000000
 /// - index 2 -> 0b00000011
@@ -62,6 +153,16 @@ const static Char s_colon(
    0b00000001,
    0b00000000,
    0b00000001,
+   0b00000001,
+   0b00000000});
+const static Char s_fullstop( 
+  1, 
+  {0b00000000,
+   0b00000000,
+   0b00000000,
+   0b00000000,
+   0b00000000,
+   0b00000000,
    0b00000001,
    0b00000000}); 
 const static Char s_one( 
@@ -183,7 +284,14 @@ static const Char digits[] = {
 struct Display
 {
   using data_t = std::array<uint64_t, display_height>;
-  
+
+  Display( MATRIX7219& m )
+    : matrix(m)
+  {
+    matrix.begin();
+    matrix.clear();  
+  }
+     
   /// extract data for one row in one matrix; matrix is
   /// zero indexed
   uint8_t get_matrix_row(uint8_t matrix, uint8_t row) const
@@ -210,30 +318,23 @@ struct Display
     rows = {};
     column_offset = 0;
   }
-  
+
+  void render()
+  {
+    for ( uint8_t m = 0; m < matrix_count; ++m )
+      for ( uint8_t r = 0; r < display_height; ++r )
+      {
+        matrix.setRow(1 + r, get_matrix_row(m, r), 1 + m);
+      }
+  }
+
+  MATRIX7219& matrix;
   data_t rows {};
   uint8_t column_offset {};
 };
 
 
-unsigned int localPort = 2390;  // local port to listen for UDP packets
-
-IPAddress timeServerIP;  // time.nist.gov NTP server address
-const char* ntpServerName = "time.nist.gov";
-
-const int NTP_PACKET_SIZE = 48;  // NTP time stamp is in the first 48 bytes of the message
-
-byte packetBuffer[NTP_PACKET_SIZE];  // buffer to hold incoming and outgoing packets
-
-// A UDP instance to let us send and receive packets over UDP
-WiFiUDP udp;
-
-static const int analogue_pin = A0;
-static const int matrix_data_pin = D7;
-static const int matrix_select_pin = D8;
-static const int matrix_clock_pin = D5;
-static const int matrix_count = 4;
-
+// matrix interface
 MATRIX7219 matrix(
   matrix_data_pin,
   matrix_select_pin,
@@ -241,17 +342,11 @@ MATRIX7219 matrix(
   matrix_count
   );
 
+// display abstraction over matrix
+Display display(matrix);
+
 // keep track of how long since last NTP query
 int ntp_query_time = -1;
-
-// keep track of time
-struct
-{
-  uint8_t h {};
-  uint8_t m {};
-  uint8_t s {};
-} 
-current_time;
 
 // default brightness: 0..15
 int brightness = 3;
@@ -279,11 +374,7 @@ constexpr std::tuple<uint8_t, uint8_t> split(uint8_t n)
 
 ///
 void setup() 
-{
-  // setup matrix
-  matrix.begin();
-  matrix.clear();
-  
+{ 
   Serial.begin(115200);
   Serial.println();
   Serial.println();
@@ -297,6 +388,8 @@ void setup()
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    display.add_char(s_fullstop);
+    display.render();
   }
   Serial.println("");
 
@@ -308,6 +401,8 @@ void setup()
   udp.begin(localPort);
   Serial.print("Local port: ");
   Serial.println(udp.localPort());
+
+  last_update_ms = millis();
 }
 
 ///
@@ -322,30 +417,45 @@ void loop()
   }
 
   // build string - right to left
-  Display d;
+  display.clear();
   auto [st, su] = split(current_time.s);
-  d.add_char(digits[su]);
-  d.add_char(digits[st]);
+  display.add_char(digits[su]);
+  display.add_char(digits[st]);
+  display.add_char(s_colon);
   auto [mt, mu] = split(current_time.m);
-  d.add_char(digits[mu]);
-  d.add_char(digits[mt]);
+  display.add_char(digits[mu]);
+  display.add_char(digits[mt]);
+  display.add_char(s_colon);
+  
   auto [ht, hu] = split(current_time.h);
-  d.add_char(digits[hu]);
-  d.add_char(digits[ht]);
- 
-  for ( uint8_t m = 0; m < 4; ++m )
-    for ( uint8_t r = 0; r < display_height; ++r )
-    {
-      matrix.setRow(1 + r, d.get_matrix_row(m, r), 1 + m);
-    }
+  display.add_char(digits[hu]);
+  display.add_char(digits[ht]);
+  display.render();
 
   // check if we need to update NTP
   int now = millis();
+
+  if (now - last_update_ms > 1000)
+  {
+    last_update_ms = now;
+    current_time.s += 1;
+    if ( current_time.s == 100 )
+    {
+      current_time.m += 1;
+      current_time.s = 0;
+    }
+    if ( current_time.m == 100 )
+    {
+      current_time.h += 1;
+      current_time.m = 0;
+    }
+  }  
   if ( should_poll_ntp(now) )
   {
     ntp_query_time = now;
     
     // get a random server from the pool
+    IPAddress timeServerIP;
     WiFi.hostByName(ntpServerName, timeServerIP);
   
     sendNTPpacket(timeServerIP);  // send an NTP packet to a time server
